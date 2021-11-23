546da2a2a0238e58db452306de0f0022b2f957fa
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-11-23 14:54:05 +0900

    Add `ip_addr_pton`

--- c/sample.c
+++ c/sample.c
@@ -1,1 +1,27 @@
+#include "sample.h"
+
+#include <stdint.h>
+
 int f(int n) { return n * 2; }
+
+int ip_addr_pton(const char *p, ip_addr_t *n) {
+  char *sp, *ep;
+  long ret;
+
+  sp = (char *)p;
+  for (int idx = 0; idx < 4; idx++) {
+    ret = strtol(sp, &ep, 10);  // 10進数
+    if (ret < 0 || ret > 255) {
+      return -1;
+    }
+    if (ep == sp) {
+      return -1;
+    }
+    if ((idx == 3 && *ep != '\0') || (idx != 3 && *ep != '.')) {
+      return -1;
+    }
+    ((uint8_t *)n)[idx] = ret;
+    sp = ep + 1;
+  }
+  return 0;
+}
--- c/sample.h
+++ c/sample.h
@@ -1,1 +1,6 @@
-int f(int);
+#include <stdint.h>
+
+typedef uint32_t ip_addr_t;
+
+extern int f(int);
+extern int ip_addr_pton(const char *p, ip_addr_t *n);
