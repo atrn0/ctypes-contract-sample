3dc6ff87ec75bcc315a027031e0d236905e1ed06
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-11-23 13:24:44 +0900

    Move generated files to gen dir

--- bindgen.ml
+++ bindgen.ml
@@ -1,7 +1,7 @@
 let () =
-  let fmt = Format.formatter_of_out_channel (open_out "sample_stubs.c") in
+  let fmt = Format.formatter_of_out_channel (open_out "gen/sample_stubs.c") in
   Format.fprintf fmt "#include \"sample.h\"\n";
   Cstubs.write_c fmt ~prefix:"caml_" (module Bindings.C);
 
-  let fmt = Format.formatter_of_out_channel (open_out "sample_generated.ml") in
+  let fmt = Format.formatter_of_out_channel (open_out "gen/sample_generated.ml") in
   Cstubs.write_ml fmt ~prefix:"caml_" (module Bindings.C)
