9a63ab7cb40da6da66d01b13f805dfcb98e09152
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-11-25 19:33:01 +0900

    Generate sample wrapper function

--- cbuf/cbuf.ml
+++ cbuf/cbuf.ml
@@ -4,7 +4,31 @@ module type FOREIGN' = FOREIGN with type 'a result = unit
 
 module type BINDINGS = functor (F : FOREIGN') -> sig end
 
+type bind = Bind : string * string * ('a -> 'b) Ctypes.fn -> bind
+
+let write_foreign fmt (bindings: bind list) =
+  Format.fprintf fmt "@[\
+open Ctypes
+
+module C = Bindings.C(Generated.Cstubs_gen)
+
+exception IP_addr_error
+@]";
+  List.iter (fun (Bind (stub_name, _, _)) ->
+    Format.fprintf fmt "@[\
+let %s (p: string): bytes =
+  let n = Bytes.create 4 in
+  let ret = C.IP.ip_addr_pton p (ocaml_bytes_start n) in
+  if ret <> 0 then raise IP_addr_error else n
+@]@."
+    stub_name
+    ) bindings
+
 let write_ml (fmt: Format.formatter) (module B : BINDINGS): unit =
+  let bindings = ref []
+  and counter = ref 0 in
+  let var name = incr counter;
+    Printf.sprintf "caml_%d_%s" !counter name in
   let foreign: (module FOREIGN') = (
     module struct
       type 'a fn = 'a Ctypes.fn
@@ -12,9 +36,11 @@ let write_ml (fmt: Format.formatter) (module B : BINDINGS): unit =
       let (@->) = Ctypes.(@->)
       let returning = Ctypes.returning
       type 'a result = unit
-      let foreign cname _ = print_endline ("executing foreign. cname: " ^ cname)
+      let foreign cname fn =
+        let name = var cname in
+        bindings := Bind (cname, name, fn) :: !bindings
       let foreign_value cname _ = print_endline ("executing foreign_value. cname: " ^ cname)
     end) in
   let module _ = B((val foreign)) in
-  Format.fprintf fmt "open Ctypes@.";
+  write_foreign fmt !bindings;
   print_endline "Cbuf.write_ml generated ml."
--- sample.ml
+++ sample.ml
@@ -1,17 +1,10 @@
-open Ctypes
-
 module C = Bindings.C(Generated.Cstubs_gen)
 
 let () = 
   let output = C.Sample.f 10 in
   print_endline (Int.to_string output)
 
-exception IP_addr_error
-let ip_addr_pton (p: string): bytes =
-  let n = Bytes.create 4 in
-  let ret = C.IP.ip_addr_pton p (ocaml_bytes_start n) in
-  if ret <> 0 then raise IP_addr_error else n
-
 let () =
-  let n = ip_addr_pton "192.168.0.255" in
-  Bytes.iter (fun c -> let i = Char.code c in print_int i; print_char '.') n;
+  let open Cbuf_gen in
+  let n = ip_addr_pton "192.168.0.1" in
+  Bytes.iter (fun c -> let i = Char.code c in print_int i; print_char '.') n
