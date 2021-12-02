da06e4562b62698079a1446128cf87cc65340a99
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-12-02 15:09:02 +0900

    wip

--- cbuf/cbuf.ml
+++ cbuf/cbuf.ml
@@ -4,25 +4,34 @@ module type FOREIGN' = FOREIGN with type 'a result = unit
 
 module type BINDINGS = functor (F : FOREIGN') -> sig end
 
+(* exception Unimplemented *)
+
+(* (cname, name, fn) *)
 type bind = Bind : string * string * ('a -> 'b) Ctypes.fn -> bind
+
+(* let write_args fmt fn =
+  let rec write_args2 : type a. Format.formatter -> a Ctypes.fn -> int -> unit = fun fmt fn counter -> 
+    match fn with 
+    | Function (_, f) -> 
+      Format.fprintf fmt "@[(v%d: %s) @]" counter "(CI.View {CI.ty = CI.Pointer _})";
+      write_args2 fmt f (counter + 1)
+    | _ -> ()
+  in
+  write_args2 fmt fn 1 *)
 
 let write_foreign fmt (bindings: bind list) =
   Format.fprintf fmt "@[\
-open Ctypes
-
 module C = Bindings.C(Generated.Cstubs_gen)
 
-exception IP_addr_error
-@]";
-  List.iter (fun (Bind (stub_name, _, _)) ->
-    Format.fprintf fmt "@[\
-let %s (p: string): bytes =
-  let n = Bytes.create 4 in
-  let ret = C.IP.ip_addr_pton p (ocaml_bytes_start n) in
-  if ret <> 0 then raise IP_addr_error else n
-@]@."
-    stub_name
-    ) bindings
+exception IP_addr_error@.";
+  let write_buf_wrapper = fun (Bind (stub_name, _, _)) ->
+    Format.fprintf fmt "let %s: string -> bytes =@." stub_name;
+    Format.fprintf fmt "  fun v1 ->@.";
+    Format.fprintf fmt "  let n = Bytes.create 4 in
+  let ret = C.IP.ip_addr_pton v1 (Ctypes.ocaml_bytes_start n) in
+  if ret <> 0 then raise IP_addr_error else n@.@]" 
+  in 
+  List.iter write_buf_wrapper bindings
 
 let write_ml (fmt: Format.formatter) (module B : BINDINGS): unit =
   let bindings = ref []
